7cc1711743aac6738cae9b8e273701d1c91c4e9b
abc <sr.samanraja@gmail.com>
2020-06-10 21:00:14 +0530

    how to add mailchimp api to store data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require('https');
 
 
 const app = express();
@@ -14,13 +15,51 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const JSONData = JSON.stringify(data);
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/10e1a44855"
+
+    const option = {
+        method: "POST",
+        auth: "saman:a5a8b8e7a859e933a41d49fe3024548f-us10"
+    }
+
+    const request = https.request(url, option, function(response) {
+       response.on("data", function(data){
+           console.log(JSON.parse(data));
+       }) 
+    })
+
+    request.write(JSONData);
+    request.end();
+
+    
 });
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+// api key 
+// a5a8b8e7a859e933a41d49fe3024548f-us10
+//a5a8b8e7a859e933a41d49fe3024548f-us10
+
+// list id
+// 10e1a44855
